8922455682a840abb873794ca840511f0143e1e8
AVINEET-Singh <avi.singh1409@gmail.com>
2024-06-10 01:16:36 -0400

    Done Competitive-Coding-4

---
+++ balancedBinaryTree_110.py
@@ -0,0 +1,21 @@
+# Time Complexity : O(n)
+# Space Complexity :  O(h), where h is height of the binary tree
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return (self.Height(root) >= 0)
+    def Height(self, root):
+        if root is None: 
+            return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  
+            return -1
+        return max(leftheight, rightheight) + 1 
